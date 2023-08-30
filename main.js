718e432c5a32199bae08f6e69619e60ad8a6de57
IsaKaillany <isakaillany123@gmail.com>
2023-08-29 23:10:39 -0300

    add estilização com tailwind

--- main.js
+++ main.js
@@ -67,13 +67,13 @@ const catalogo = [
 
 for (const produtoCatalogo of catalogo) {
     const cartaoProduto = `
-        <div id="cardProduto1">
+        <div id="cardProduto${produtoCatalogo.id}" class="border-solid border-2 border-sky-500 w-48 m-2">
             <img src="./assets/img/${produtoCatalogo.imagem}" style="height: 300px" alt="Produto 1">
-        </div>
-        <p>${produtoCatalogo.marca}</p>
-        <p>${produtoCatalogo.nome}</p>
-        <p>$${produtoCatalogo.preco}</p>
-        <button>Adicionar</button>`;
+            <p class="marca">${produtoCatalogo.marca}</p>
+            <p>${produtoCatalogo.nome}</p>
+            <p>$${produtoCatalogo.preco}</p>
+            <button>Adicionar</button>
+        </div>`;
     
     document.getElementById('containerProduto').innerHTML += cartaoProduto;
 };
