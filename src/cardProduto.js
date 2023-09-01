d36540abf372b2ac94a167703762f1252d85475e
IsaKaillany <isakaillany123@gmail.com>
2023-09-01 20:44:32 -0300

    Aula 3

--- src/cardProduto.js
+++ src/cardProduto.js
@@ -1,3 +1,4 @@
+import { adicionarAoCarrinho } from "./menuCarrinho";
 import { catalogo } from "./utilidades";
 
 export function renderizarCatalogo() {
@@ -8,9 +9,16 @@ export function renderizarCatalogo() {
                 <p class="text-sm">${produtoCatalogo.marca}</p>
                 <p class="text-sm">${produtoCatalogo.nome}</p>
                 <p class="text-sm">$${produtoCatalogo.preco}</p>
-                <button class="bg-slate-950 hover:bg-slate-700 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
+                <button id="adicionar${produtoCatalogo.id}" class="bg-slate-950 hover:bg-slate-700 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
             </div>`;
 
-    document.getElementById("containerProduto").innerHTML += cartaoProduto;
+        document.getElementById("containerProduto").innerHTML += cartaoProduto;
+    }
+    for (const produtoCatalogo of catalogo) {
+        document
+            .getElementById(`adicionar${produtoCatalogo.id}`)
+            .addEventListener("click", () =>
+                adicionarAoCarrinho(produtoCatalogo.id)
+            );
     }
 }
