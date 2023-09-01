529380ef9a3ff2706a9dfb640c575cdaa0d0151b
IsaKaillany <isakaillany123@gmail.com>
2023-09-01 20:28:03 -0300

    add +estilização

--- src/cardProduto.js
+++ src/cardProduto.js
@@ -3,12 +3,12 @@ import { catalogo } from "./utilidades";
 export function renderizarCatalogo() {
     for (const produtoCatalogo of catalogo) {
         const cartaoProduto = `
-            <div id="cardProduto${produtoCatalogo.id}" class="border-solid border-2 border-sky-500 w-48 m-2">
-                <img src="./assets/img/${produtoCatalogo.imagem}" style="height: 300px" alt="Produto 1">
-                <p class="marca">${produtoCatalogo.marca}</p>
-                <p>${produtoCatalogo.nome}</p>
-                <p>$${produtoCatalogo.preco}</p>
-                <button>Adicionar</button>
+            <div id="cardProduto${produtoCatalogo.id}" class="border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group">
+                <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto ${produtoCatalogo.id}" class="group-hover:scale-110 duration-300 my-3 rounded-lg">
+                <p class="text-sm">${produtoCatalogo.marca}</p>
+                <p class="text-sm">${produtoCatalogo.nome}</p>
+                <p class="text-sm">$${produtoCatalogo.preco}</p>
+                <button class="bg-slate-950 hover:bg-slate-700 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
             </div>`;
 
     document.getElementById("containerProduto").innerHTML += cartaoProduto;
