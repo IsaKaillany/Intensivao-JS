b42faf2ac1341328302d5b4f5e2ba53296cde38b
IsaKaillany <isakaillany123@gmail.com>
2023-09-12 23:25:25 -0300

    Aula 05 - 32:45

--- src/cardProduto.js
+++ src/cardProduto.js
@@ -4,7 +4,7 @@ import { catalogo } from "./utilidades";
 export function renderizarCatalogo() {
     for (const produtoCatalogo of catalogo) {
         const cartaoProduto = `
-            <div id="cardProduto${produtoCatalogo.id}" class="border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group">
+            <div id="cardProduto${produtoCatalogo.id}" class="border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group ${produtoCatalogo.feminino ? 'feminino' : 'masculino'}">
                 <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto ${produtoCatalogo.id}" class="group-hover:scale-110 duration-300 my-3 rounded-lg">
                 <p class="text-sm">${produtoCatalogo.marca}</p>
                 <p class="text-sm">${produtoCatalogo.nome}</p>
